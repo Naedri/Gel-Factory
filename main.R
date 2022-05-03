1cb8fbc19c2a41ce2041b2fc724fad7e41a9a87a
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-05-03 21:53:57 +0200

    refactor: add_feat and t(canvas)

--- main.R
+++ main.R
@@ -1,18 +1,21 @@
+# cleaning
+rm(list = ls(all = TRUE))
+data.frame(unlist("all"))
+
 # function
 generate_feature <- function(height, width, minValue, maxValue) {
   y <- dgamma(seq(0, 25, by = 25 / (height - 1)), 2, 0.2)
-  y <- y
   xy <- matrix(NA, height, width)
   
   for (pos in seq(height)) {
     x <- dnorm(seq(-1.5, 1.5, by = 3 / (width - 1)), 0, y[pos] * 2)
     x <- x / max(x) * y[pos]
     x[is.nan(x)] <- 0
-    xy[pos, ] <- x
+    xy[pos,] <- x
   }
   xy <- scale_feature(xy, minValue, maxValue)
   
-  return(xy)
+  return(t(xy))
 }
 
 
@@ -24,28 +27,18 @@ scale_feature <- function(feature, minValue, maxValue) {
   return(feature)
 }
 
-draw_feature <- function(canvas,
-                         feature,
-                         yStart = 0,
-                         xStart = 0) {
+draw_feature <- function(canvas, feature, yStart, xStart) {
   dimCanvas <- dim(canvas)
   dimFeature <- dim(feature)
   
-  if (yStart < 1 | yStart > dimCanvas[1]) {
-    yStart <- sample(1:dimCanvas[1], 1)
-  }
-  if (xStart < 1 | xStart > dimCanvas[2]) {
-    xStart <- sample(1:dimCanvas[2], 1)
-  }
-  
   for (y in 1:dimFeature[1]) {
-    yPos <- yStart + y - 1
-    if (yPos > dimCanvas[1]) {
+    yPos <- yStart + y
+    if (yPos < 1 | yPos > dimCanvas[1]) {
       next
     }
     for (x in 1:dimFeature[2]) {
-      xPos <- xStart + x - 1
-      if (xPos > dimCanvas[2]) {
+      xPos <- xStart + x
+      if (xPos < 1 | xPos > dimCanvas[2]) {
         next
       }
       canvas[yPos, xPos] <-
@@ -57,25 +50,32 @@ draw_feature <- function(canvas,
 }
 
 
-add_feat <- function(canvas, nfeat, nlong, min, max) {
+add_feat <- function(canvas, pointFeat, lineFeat, min, max) {
   dimCanvas <- dim(canvas)
   
-  for (i in 1:nfeat) {
-    feature <-
-      generate_feature(sample((dimCanvas[1] / 20):(dimCanvas[1] / 10), 1),
-                       sample((dimCanvas[2] / 20):(dimCanvas[2] / 5), 1),
-                       min, max)
-    canvas <- draw_feature(canvas, feature)
+  
+  if (pointFeat > 0) {
+    for (i in 1:pointFeat) {
+      height <- sample((dimCanvas[1] / 20):(dimCanvas[1] / 10), 1)
+      width <- sample((dimCanvas[2] / 20):(dimCanvas[2] / 5), 1)
+      feature <- generate_feature(height, width, min, max)
+      
+      yStart <- sample(0:dimCanvas[1], 1)
+      xStart <- sample(0:dimCanvas[2], 1)
+      canvas <- draw_feature(canvas, feature, yStart, xStart)
+    }
   }
   
-  for (i in 1:nlong) {
-    feature <-
-      generate_feature(sample((dimCanvas[1] * 1.5):(dimCanvas[1] * 2), 1),
-                       sample((dimCanvas[2] / 20):(dimCanvas[2] / 10), 1),
-                       min, max)
-    yStart <- sample(1:(dimCanvas[1] / 8), 1)
-    xStart <- sample(1:(dimCanvas[2] / 3), 1)
-    canvas <- draw_feature(canvas, feature, xStart, yStart)
+  if (lineFeat > 0) {
+    for (i in 1:lineFeat) {
+      height <- sample((dimCanvas[1] * 1.5):(dimCanvas[1] * 2), 1)
+      width <- sample((dimCanvas[2] / 20):(dimCanvas[2] / 10), 1)
+      feature <- generate_feature(height, width, min, max)
+      
+      yStart <- sample(0:(dimCanvas[1] / 8), 1)
+      xStart <- sample(0:(dimCanvas[2] / 3), 1)
+      canvas <- draw_feature(canvas, feature, yStart, xStart)
+    }
   }
   
   return(canvas)
@@ -95,7 +95,7 @@ min <- 230 # minimal Value
 max <- 65536 # maximal Value
 lengthColor <- 256 # RGB flavours
 featureLong <- 3
-featurePoint <- 15 - featureLong
+featurePoint <- 12
 
 # gradient from brewer "Spectral" palette
 colfunc <- colorRampPalette(c("#3288BD", "#FFFFBF", "#D53E4F"))
@@ -108,4 +108,4 @@ canvas <- add_feat(canvas, featurePoint, featureLong, min, max)
 
 # export
 #image_export(feature, "output/Rplot", gradient)
-image_export(t(canvas), "output/Rplot", gradient)
+image_export(canvas, "output/Rplot", gradient)
